37a322ad9afaa2290cd4ebbca99f96c1f0d77412
growtechaxon <growtechaxon@gmail.com>
2026-09-11 09:34:42 +0530

    Connect frontend to production backend

--- js/script.js
+++ js/script.js
@@ -427,7 +427,7 @@ document.addEventListener("DOMContentLoaded", () => {
         try {
 
             const response = await fetch(
-                "http://localhost:5000/api/leads",
+                "https://growtechaxon-backend.onrender.com"
                 {
                     method: "POST",
 
