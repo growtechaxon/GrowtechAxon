45ca02fce10b64d63f6449369db4850b6fc2732d
growtechaxon <growtechaxon@gmail.com>
2026-09-12 08:38:36 +0530

    Update website

--- admin/admin.js
+++ admin/admin.js
@@ -10,10 +10,7 @@
 ========================================================= */
 
 const API_URL =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-        ? "https://growtechaxon-backend.onrender.com"
-        : window.location.origin;
+    "https://growtechaxon-backend.onrender.com";
 
 
 /* =========================================================
